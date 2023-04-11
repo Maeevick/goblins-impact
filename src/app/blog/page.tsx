d5eb859e23468232ba42f122d0e050c51a2e4e8e
Aurel (Maeevick) <maeevick@gmail.com>
2023-04-11 12:48:31 +0200

    feat: blog disclaimer

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -15,6 +15,14 @@ export default function Blog() {
     <>
       <Header description={`${BLOG_DATA.header}`} action={<HomeLink />} />
       <PageTitle title={`${BLOG_DATA.title}...`} />
+      <article className="pb-20 text-center">
+        <h3 className="text-xl my-2 underline">Information/Disclaimer</h3>
+        <p>
+          Pour les puristes de la langue française, je suis désolé d&apos;avance
+          mais ce blog est écrit en <em>fran-glais</em>{" "}
+          (&#127467;&#127479;-&#127468;&#127463;).
+        </p>
+      </article>
       <CardLayout data={getBlogPosts()} />
     </>
   );
