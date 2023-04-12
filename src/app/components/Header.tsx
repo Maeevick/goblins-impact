21592c2b4788e14803ab88413e602946d3f6530d
Aurel (Maeevick) <maeevick@gmail.com>
2023-04-12 22:30:28 +0200

    fix: responsive header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -8,7 +8,7 @@ export type HeaderProps = {
 export default function Header({ description, action }: HeaderProps) {
   return (
     <header className="w-full flex items-center justify-between">
-      <div className="flex-none max-w-[120px] md:max-w-fit border border-orange-500 rounded-lg p-2 m-2 text-sm">
+      <div className="flex-none max-w-[150px] md:max-w-fit border border-orange-500 rounded-lg p-2 m-2 text-sm">
         {description}
       </div>
       <div className="flex-none p-2 m-2">{action}</div>
